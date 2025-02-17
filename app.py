8fd7722f79fc763c162acffc6136ba179b0e48cf
Gokul RM <117154988+GokulRajiMahendiran@users.noreply.github.com>
2025-02-18 01:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -168,4 +168,5 @@ def update_dashboard(n):
         timestamp
     )
 if __name__ == "__main__":
-    app.run_server(debug=True, port=8050)
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug=True, host="0.0.0.0", port=port)
